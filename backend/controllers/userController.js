da46622f4f4d05ff30ff7af5ff06e26bb28e1fa8
Kiran M <kiran7619401943@gmail.com>
2026-02-20 17:24:42 +0530

    CI/CD test for backend 1

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,7 @@ exports.registerUser = async (req, res) => {
 
     await newUser.save();
 
-    res.status(201).json({ message: "User registered success" });
+    res.status(201).json({ message: "User registered ok" });
 
   } catch (error) {
     console.error(error);
